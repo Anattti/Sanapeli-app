3edfabea7f7739b86792fb8098dcb1f96d948510
Antti Lampi <antsa_93_@hotmail.com>
2025-11-07 11:36:22 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
